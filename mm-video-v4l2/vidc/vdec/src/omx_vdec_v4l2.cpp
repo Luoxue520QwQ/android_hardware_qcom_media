ba99544cfa4dc9bedacafec75a08684766530e98
Luoxue <luoxue520owo@qq.com>
2025-12-16 23:00:30 +0800

    Refactor MISR debug print statements for clarity

--- mm-video-v4l2/vidc/vdec/src/omx_vdec_v4l2.cpp
+++ mm-video-v4l2/vidc/vdec/src/omx_vdec_v4l2.cpp
@@ -11426,17 +11426,17 @@ bool omx_vdec::handle_extradata(OMX_BUFFERHEADERTYPE *p_buf_hdr)
                             m_extradata_info.output_crop_updated = OMX_TRUE;
                             for(unsigned int m=0; m<output_crop_payload->misr_info[0].misr_set; m++) {
                             DEBUG_PRINT_HIGH("MISR0: %x %x %x %x\n",
-                                output_crop_payload->misr_info[0].misr_dpb_luma[m],
-                                output_crop_payload->misr_info[0].misr_dpb_chroma[m],
-                                output_crop_payload->misr_info[0].misr_opb_luma[m],
-                                output_crop_payload->misr_info[0].misr_opb_chroma[m]);
+                                output_crop_payload->misr_info[0].misr_dpb_luma,
+                                output_crop_payload->misr_info[0].misr_dpb_chroma,
+                                output_crop_payload->misr_info[0].misr_opb_luma,
+                                output_crop_payload->misr_info[0].misr_opb_chroma);
                             }
                             for(unsigned int m=0; m< output_crop_payload->misr_info[1].misr_set; m++) {
                                 DEBUG_PRINT_HIGH("MISR1: %x %x %x %x\n",
-                                                 output_crop_payload->misr_info[1].misr_dpb_luma[m],
-                                                 output_crop_payload->misr_info[1].misr_dpb_chroma[m],
-                                                 output_crop_payload->misr_info[1].misr_opb_luma[m],
-                                                 output_crop_payload->misr_info[1].misr_opb_chroma[m]);
+                                   output_crop_payload->misr_info[1].misr_dpb_luma,
+                                   output_crop_payload->misr_info[1].misr_dpb_chroma,
+                                   output_crop_payload->misr_info[1].misr_opb_luma,
+                                   output_crop_payload->misr_info[1].misr_opb_chroma);
                             }
                             memcpy(m_extradata_info.misr_info, output_crop_payload->misr_info, 2 * sizeof(msm_vidc_misr_info));
                             if (client_extradata & OMX_OUTPUTCROP_EXTRADATA) {
